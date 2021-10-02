dbf1350bfae16ffba5124b8b1b38aa789ef936a1
SenKill <serik1404500@gmail.com>
2021-10-02 11:01:39 +0600

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Serik Musaev on 10/2/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
